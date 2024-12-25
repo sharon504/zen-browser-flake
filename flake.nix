8ef25ba71e0c477c40eef636160e991408cd666b
Omar Crespo <crespomerchano@gmail.com>
2024-12-25 14:22:01 -0500

    update script to match new zen specific & generic deprecation

--- flake.nix
+++ flake.nix
@@ -10,10 +10,10 @@
       system = "x86_64-linux";
       version = "1.0.2-b.3";
       downloadUrl = {
-        specific.url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-specific.tar.bz2";
+        specific.url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.bz2";
         specific.sha256 = "0gjrvsq83l6424ijii2w0c43f2nkf6n04hb2bc9wf1yyq7g3s2nc";
 
-        generic.url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-generic.tar.bz2";
+        generic.url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.bz2";
         generic.sha256 = "1kv44fkql60rjgqcqsfdhbi4zr8bi91fkswlsk5d6mwj8nw1clmj";
       };
 
