b0e5c31ad7e12a2682693de9b4698d5c0f8d3743
omarcresp <27465620+omarcresp@users.noreply.github.com>
2025-03-19 00:15:58 +0000

    GitHub Action automated Zen update

--- flake.nix
+++ flake.nix
@@ -8,13 +8,13 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      version = "1.10b";
+      version = "1.9.1b";
       downloadUrl = {
         specific.url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.xz";
-        specific.sha256 = "037d2c9c4qx4hcxy9qf6d22lwf21mj6k1r8gjnwsvcwvard3awjy";
+        specific.sha256 = "073lw306rffhr8nz8psdmdrc0vb2bmz6j02i5iabv0dyc190nk9z";
 
         generic.url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.xz";
-        generic.sha256 = "037d2c9c4qx4hcxy9qf6d22lwf21mj6k1r8gjnwsvcwvard3awjy";
+        generic.sha256 = "073lw306rffhr8nz8psdmdrc0vb2bmz6j02i5iabv0dyc190nk9z";
       };
 
       pkgs = import nixpkgs {
