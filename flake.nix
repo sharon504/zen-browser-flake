deb12c354b5026495660d15b1cea2677c5fd609b
omarcresp <27465620+omarcresp@users.noreply.github.com>
2025-04-15 23:15:53 +0000

    GitHub Action automated Zen update

--- flake.nix
+++ flake.nix
@@ -8,13 +8,13 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      version = "1.11.3b";
+      version = "1.11.2b";
       downloadUrl = {
         specific.url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.xz";
-        specific.sha256 = "06d6nd0yzz1r73w2qxyawcbbfnc5wyar28vxqpi8qn0gngj4jyh4";
+        specific.sha256 = "0azgzs3la1hpk7lnnvi8bc7m5m8isrgh5yqg9z8qvlszs5n9bmnx";
 
         generic.url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.xz";
-        generic.sha256 = "06d6nd0yzz1r73w2qxyawcbbfnc5wyar28vxqpi8qn0gngj4jyh4";
+        generic.sha256 = "0azgzs3la1hpk7lnnvi8bc7m5m8isrgh5yqg9z8qvlszs5n9bmnx";
       };
 
       pkgs = import nixpkgs {
